25dca61985666abbf3c69f6be7fefbbb224b3436
Dawid Jaloza <dawidjaloza@gmail.com>
2020-05-14 19:37:37 +0200

    Version no.2 recognising words in sentences

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -20,21 +20,23 @@ public class Main {
         locations.get(1).addExit("E", 3);
         locations.get(1).addExit("S", 4);
         locations.get(1).addExit("N", 5);
-//        locations.get(1).addExit("Q", 0);
 
         locations.get(2).addExit("N", 5);
-//        locations.get(2).addExit("Q", 0);
 
         locations.get(3).addExit("W", 1);
-//        locations.get(3).addExit("Q", 0);
 
         locations.get(4).addExit("N", 1);
         locations.get(4).addExit("W", 2);
-//        locations.get(4).addExit("Q", 0);
 
         locations.get(5).addExit("S", 1);
         locations.get(5).addExit("W", 2);
-//        locations.get(5).addExit("Q", 0);
+
+        Map<String, String> vocabulary = new HashMap<>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
 
         int loc = 1;
         while(true) {
@@ -51,22 +53,33 @@ public class Main {
             System.out.println();
 
             String direction = scanner.nextLine().toUpperCase();
-//            ==========================
-//            Version no.1 recognising words in sentences
-            String[] cutDirection = direction.split(" ");
-            for (String i : cutDirection) {
-                if (i.equals("WEST")) {
-                    direction = "W";
-                } else if (i.equals("EAST")) {
-                    direction = "E";
-                } else if (i.equals("NORTH")) {
-                    direction = "N";
-                } else if (i.equals("SOUTH")) {
-                    direction = "S";
-                } else if (i.equals("EXIT") || i.equals("QUIT")) {
-                    direction = "Q";
+
+            if(direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for(String word : words) {
+                    if(vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
                 }
             }
+
+//            ==========================
+//            Version no.1 recognising words in sentences
+//            String[] cutDirection = direction.split(" ");
+//            for (String i : cutDirection) {
+//                if (i.equals("WEST")) {
+//                    direction = "W";
+//                } else if (i.equals("EAST")) {
+//                    direction = "E";
+//                } else if (i.equals("NORTH")) {
+//                    direction = "N";
+//                } else if (i.equals("SOUTH")) {
+//                    direction = "S";
+//                } else if (i.equals("EXIT") || i.equals("QUIT")) {
+//                    direction = "Q";
+//                }
+//            }
 //            ===========================
             if(exits.containsKey(direction)) {
                 loc = exits.get(direction);
